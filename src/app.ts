7ca96b41102d1ceadde23868be49a46c193ab85b
Luis Fernando M <luisfernandomgrs@gmail.com>
2021-10-18 23:32:05 -0300

    code implementation start to project, all functionally application

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express from "express";
+import { router } from "./routes";
+
+const app = express();
+const myPortAccess = 8080;
+
+app.use(express.json());
+app.use(router);
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+})
+
+app.listen(myPortAccess, () => console.log(`🚀 Server is running on PORT ${myPortAccess}`));
